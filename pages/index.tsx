2020372d60bb823fb3f732ead0aebcbedae3357a
Ben <benkallaus@gmail.com>
2022-05-18 19:33:27 -0500

    feat: including menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,93 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { TextField } from '@mui/material';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { TextField } from "@mui/material";
 
-import React from 'react';
-import {seats} from '../src/seats';
-
+import React from "react";
+import { seats } from "../src/seats";
 
 const Home: NextPage = () => {
-  const [name, setName] = React.useState('');
- 
+  const [name, setName] = React.useState("");
+
   return (
     <div>
       <Head>
-        <title>Ben and Michelle Guest Seating</title>
+        <title>Ben and Michelle Menu and Guest Seating</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container maxWidth="sm">
-      <Typography variant="h4" component="h1" gutterBottom>
-          Ben and Michelle Guest Seating
-      </Typography>
-      <Box sx={{ my: 4 }}>
-        <TextField label='Enter Guest Name' onChange={(e) => setName(e.target.value)} value={name}/>
-        {
-          seats.filter((seat) => {
-            return seat.firstName.toLowerCase().includes(name.toLowerCase()) || seat.lastName.toLowerCase().includes(name.toLowerCase());
-          }).map((seat) => {
-            return <Box>
-                  <Typography>{seat.firstName} {seat.lastName} - {seat.tableNumber}</Typography>
-              </Box>;
-          })
-        }
-      </Box>
-    </Container>
+        <Typography pt={4} fontSize={24} fontWeight="800">
+          Menu and Guest Seating
+        </Typography>
+        <Typography fontSize={14}>
+          We're so happy you could make it to our wedding!
+        </Typography>
+        <Box pt={4}>
+          <Typography fontSize={16} fontWeight={800}>
+            Menu
+          </Typography>
+          <Typography fontSize={16} fontWeight={500}>
+            SOUP
+          </Typography>
+          <Typography fontSize={14}>Chicken Florentine</Typography>
+          <Typography fontSize={16} fontWeight={500}>
+            SALAD
+          </Typography>
+          <Typography fontSize={14}>House Tossed in Cucumber Bowl</Typography>
+          <Typography fontSize={16} fontWeight={500}>
+            ENTRÉE CHOICE
+          </Typography>
+          <Typography fontSize={14}>
+            Steak Delmonico* Chicken Breast Limóne* Baked New England Cod*
+          </Typography>
+          <Typography fontSize={14}>
+            *All entrees are accompanied by Garlic Mashed Potatoes and String
+            Bean Almandine
+          </Typography>
 
+          <Typography fontSize={18} fontWeight={500}>
+            DESSERT
+          </Typography>
+          <Typography fontSize={14}>
+            Tuscan Bread Pudding Wedding Cake Les Patisseries Sweets
+          </Typography>
+          <Typography fontSize={16} fontWeight={500} pt={4}>
+            **Please remind the staff of any dietary restrictions**
+          </Typography>
+        </Box>
+        <Box pt={4}>
+          <Typography fontSize={16} fontWeight={800}>
+            Guest List
+          </Typography>
+          <TextField
+            fullWidth
+            sx={{ my: 2 }}
+            label="Enter Guest Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          {seats
+            .filter((seat) => {
+              return (
+                seat.firstName.toLowerCase().includes(name.toLowerCase()) ||
+                seat.lastName.toLowerCase().includes(name.toLowerCase())
+              );
+            })
+            .map((seat) => {
+              return (
+                <Box key={`${seat.firstName}-${seat.lastName}`} py={1}>
+                  <Typography>
+                    {seat.firstName} {seat.lastName} - {seat.tableNumber}
+                  </Typography>
+                </Box>
+              );
+            })}
+        </Box>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
